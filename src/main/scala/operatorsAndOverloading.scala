5aa80013d90ff46b5aeb332e12f1b2602d36e760
romulovieira777 <romulo.vieira777@gmail.com>
2021-09-03 16:38:56 -0300

    Operations Operators

---
+++ src/main/scala/operatorsAndOverloading.scala
@@ -0,0 +1,26 @@
+object operatorsAndOverloading extends App {
+
+  println(4 + 6)
+  println(4.+(6))
+  println(6 % 4)
+  println(6 > 4)
+  println(8 < 7)
+
+  val t = true
+  println(t)
+
+  val f = false
+  println(f)
+
+  val test = t || f
+  println(test)
+
+  println(!f)
+  println(1 | 2)
+
+  val num = 3 + 5L
+  println(num)
+
+  println(5.55d + 2.2f)
+
+}
